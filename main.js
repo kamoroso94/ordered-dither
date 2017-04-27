c3c4f63b658a59ca6d8b146890736527018d85bf
Kyle Amoroso <kamoroso1994@gmail.com>
2017-04-27 06:34:51 -0400

    Styled buttons and replaced tabs

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let canvas, ctx, form, formButtons, statusBox, hint, output, downloadBtn, downloadTag;
+let canvas, ctx, form, uploadLbl, uploader, formButtons, statusBox, hint, output, downloadBtn;
 const ditherWorker = new Worker("worker.js");
 const img = new Image();
 
@@ -10,87 +10,104 @@ const requestFullscreen = ["requestFullscreen", "webkitRequestFullscreen", "mozR
 const fullscreenerror = ["fullscreenerror", "webkitfullscreenerror", "mozfullscreenerror", "msfullscreenerror"].find(eventName => "on" + eventName in document);
 
 window.addEventListener("load", function() {
-	canvas = document.querySelector("canvas");
-	ctx = canvas.getContext("2d");
-	form = document.getElementById("dither");
-	formButtons = document.getElementById("formButtons");
-	statusBox = document.getElementById("processing");
-	hint = document.getElementById("hint");
-	output = document.getElementById("output");
-	downloadBtn = document.getElementById("downloadBtn");
-	downloadTag = document.getElementById("download");
+    canvas = document.querySelector("canvas");
+    ctx = canvas.getContext("2d");
+    form = document.getElementById("dither");
+	uploadLbl = document.getElementById("uploadLbl");
+	uploader = document.getElementById("uploader");
+    formButtons = document.getElementById("formButtons");
+    statusBox = document.getElementById("processing");
+    hint = document.getElementById("hint");
+    output = document.getElementById("output");
+    downloadBtn = document.getElementById("download");
 
-	img.addEventListener("load", draw);
+    img.addEventListener("load", draw);
 
-	form.addEventListener("submit", function(e) {
-		e.preventDefault();
-		const reader = new FileReader();
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const reader = new FileReader();
+		const file = uploader.files[0];
 
-		reader.addEventListener("load", function() {
-			if(img.src != this.result) {
-				img.src = this.result;
-			} else {
-				draw();
-			}
-		});
+		if(file == null) {
+			return;
+		}
 
-		reader.readAsDataURL(document.getElementById("uploader").files[0]);
+		const name = file.name || "ditheredImage";
+		downloadBtn.download = name.replace(/\.[^.]+$/, "");
 
-		// update UI
-		statusBox.classList.remove("hidden");
-		hint.classList.add("hidden");
-		output.classList.add("hidden");
-		formButtons.setAttribute("disabled", true);
-	});
+        reader.addEventListener("load", function() {
+            if(img.src != this.result) {
+                img.src = this.result;
+            } else {
+                draw();
+            }
+        });
 
-	form.addEventListener("reset", function() {
-		hint.classList.remove("hidden");
-		output.classList.add("hidden");
-	});
+        reader.readAsDataURL(uploader.files[0]);
 
-	canvas.addEventListener("click", function() {
-		if(document[fullscreenElement]) {
-			document[exitFullscreen]();
-			this.setAttribute("title", "Open preview");
+        // update UI
+        statusBox.classList.remove("hidden");
+        hint.classList.add("hidden");
+        output.classList.add("hidden");
+        formButtons.setAttribute("disabled", true);
+    });
+
+    form.addEventListener("reset", function() {
+        hint.classList.remove("hidden");
+        output.classList.add("hidden");
+		uploadLbl.textContent = "Upload";
+    });
+
+	uploader.addEventListener("change", function() {
+		const file = uploader.files[0];
+		if(file == null) {
+			uploadLbl.textContent = "Upload";
 		} else {
-			output[requestFullscreen]();
-			this.setAttribute("title", "Close preview");
+			uploadLbl.textContent = file.name || "Uploaded";
 		}
 	});
 
-	document.addEventListener(fullscreenerror, function() {
-		canvas.setAttribute("title", "Open preview");
-	});
+    canvas.addEventListener("click", function() {
+        if(document[fullscreenElement]) {
+            document[exitFullscreen]();
+            this.setAttribute("title", "Open preview");
+        } else {
+            document.getElementById("fullscreen")[requestFullscreen]();
+            this.setAttribute("title", "Close preview");
+        }
+    });
 
-	downloadBtn.addEventListener("click", function() {
-		downloadTag.href = canvas.toDataURL();
-		downloadTag.download = "ditheredImage";
-		downloadTag.click();
-	});
+    document.addEventListener(fullscreenerror, function() {
+        canvas.setAttribute("title", "Open preview");
+    });
 
-	ditherWorker.addEventListener("message", function(event) {
-		const imageData = event.data;
-		drawCanvas(imageData, "putImageData");
+    downloadBtn.addEventListener("click", function() {
+        this.href = canvas.toDataURL();
+    });
 
-		// update UI
-		statusBox.classList.add("hidden");
-		output.classList.remove("hidden");
-		formButtons.removeAttribute("disabled");
-	});
+    ditherWorker.addEventListener("message", function(event) {
+        const imageData = event.data;
+        drawCanvas(imageData, "putImageData");
+
+        // update UI
+        statusBox.classList.add("hidden");
+        output.classList.remove("hidden");
+        formButtons.removeAttribute("disabled");
+    });
 });
 
 function draw() {
-	drawCanvas(img, "drawImage");
-	const imageData = ctx.getImageData(0, 0, img.width, img.height);
+    drawCanvas(img, "drawImage");
+    const imageData = ctx.getImageData(0, 0, img.width, img.height);
 
-	ditherWorker.postMessage({
-		imageData,
-		ditherId: document.getElementById("bitdepth").value
-	}, [imageData.data.buffer]);
+    ditherWorker.postMessage({
+        imageData,
+        ditherId: document.getElementById("bitdepth").value
+    }, [imageData.data.buffer]);
 }
 
 function drawCanvas(img, methodName) {
-	canvas.width = img.width;
-	canvas.height = img.height;
-	ctx[methodName](img, 0, 0);
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx[methodName](img, 0, 0);
 }
